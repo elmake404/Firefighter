d1c32f3d3680eacb605d9447fad53b959c56a7e4
Valentin <kibirevvalik202@gmail.com>
2020-10-21 13:21:45 +0300

    [update] Level1

--- FirefighterGame/Assets/Scripts/BurningRoom.cs
+++ FirefighterGame/Assets/Scripts/BurningRoom.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class BurningRoom : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _window;
+    private MeshRenderer _window;
+    [SerializeField]
+    private Collider _colliderMain;
     [SerializeField]
     private ParticleSystem _FbxSmoke, _fbxFire;
 
     public void ActivationFair()
     {
-        _window.SetActive(false);
+        _colliderMain.isTrigger = true;
+        _window.enabled = false;
         _fbxFire.Play();
         _FbxSmoke.Play();
     }
@@ -19,6 +22,16 @@ public class BurningRoom : MonoBehaviour
     {
         _fbxFire.Stop();
         _FbxSmoke.Stop();
-
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer==4)
+        {
+            _fbxFire.transform.localScale = Vector3.Lerp(_fbxFire.transform.localScale, Vector3.zero, 0.1f);
+            if (_fbxFire.transform.localScale.x<=0.1)
+            {
+                PutOut();
+            }
+        }
     }
 }
--- FirefighterGame/Assets/Scripts/Hydrant.cs
+++ FirefighterGame/Assets/Scripts/Hydrant.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class Hydrant : MonoBehaviour
 {
-    //private Camera _cam;
-    //private Ray _ray;
-    private Vector3 /*_posHydrant,*/ _startPosHydrant,_startPosAim;
+    private Vector3 _startPosHydrant, _startPosAim;
     [SerializeField]
     private Transform _aim, _posFair;
     [SerializeField]
@@ -14,33 +12,20 @@ public class Hydrant : MonoBehaviour
 
     [SerializeField]
     private float _forseWater, _factorDistance;
-    //private float _distensFromCamra;
     void Start()
     {
-        //_cam = Camera.main;
-        //_posHydrant = transform.position;
         _startPosAim = _aim.transform.position;
         _startPosHydrant = transform.position;
-        //_distensFromCamra = _cam.transform.position.z - transform.position.z;
 
         StartCoroutine(TurnOnTheWater());
     }
-    //void Update()
-    //{
-    //    //if (Input.GetMouseButton(0))
-    //    //{
-    //    //    _ray = _cam.ScreenPointToRay(Input.mousePosition);
-    //    //    _posHydrant =/* _startPosHydrant -*/ (_cam.transform.position - ((_ray.direction) *
-    //    //                    ((_distensFromCamra) / _ray.direction.z)));
-    //    //    Debug.Log(_posHydrant.x);
-    //    //}
-    //}
-
     void FixedUpdate()
     {
-        float posX = (_startPosAim.x - _aim.transform.position.x)* _factorDistance;
+        float posX = (_startPosAim.x - _aim.transform.position.x) * _factorDistance;
         Vector3 pos = new Vector3(_startPosHydrant.x + posX, _startPosHydrant.y, transform.position.z);
+
         transform.position = Vector3.Lerp(transform.position, pos, 0.5f);
+
         transform.LookAt(_aim.position);
     }
     private IEnumerator TurnOnTheWater()
