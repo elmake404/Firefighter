8a62e24735ae9a250e1275678eaf2c243d476552
Valentin <kibirevvalik202@gmail.com>
2020-10-26 15:03:47 +0300

    [update] Arsonist

--- FirefighterGame/Assets/Scripts/Building/Arsonist.cs
+++ FirefighterGame/Assets/Scripts/Building/Arsonist.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Arsonist : MonoBehaviour
 {
+    [System.Serializable]
+    private struct Floor
+    {
+        public Transform Stage;
+        public List<BurningRoom> BurningRooms;
+        public int ArsonisNamber;
+    }
+
     [SerializeField]
-    private BurningRoom[] _burningRooms;
-    [SerializeField]
-    private int _ArsonisNamber;
+    private Floor[] _floors;
     void Start()
     {
-        if (_ArsonisNamber>_burningRooms.Length)
-        {
-            Debug.LogError("request exceeds the number of rooms, maximum number of rooms " + _burningRooms.Length);
-        }
 
         RandomArsonis();
     }
@@ -24,20 +26,40 @@ public class Arsonist : MonoBehaviour
     }
     private void RandomArsonis()
     {
-        List <BurningRoom> ArsonisRooms = new List<BurningRoom>();
-        while (ArsonisRooms.Count<_ArsonisNamber)
+        for (int i = 0; i < _floors.Length; i++)
         {
-            int namberRoom = Random.Range(0, _burningRooms.Length);
-            if (!ArsonisRooms.Contains(_burningRooms[namberRoom]))
+            List<BurningRoom> ArsonisRooms = new List<BurningRoom>();
+
+            while (ArsonisRooms.Count < _floors[i].ArsonisNamber)
             {
-                ArsonisRooms.Add(_burningRooms[namberRoom]);
-                _burningRooms[namberRoom].ActivationFair();
+                if (_floors[i].ArsonisNamber > _floors[i].BurningRooms.Count)
+                {
+                    Debug.Log("request exceeds the number of rooms, maximum number of rooms " + _floors[i].BurningRooms.Count + " namber "+i);
+                    break;
+                }
+
+                int namberRoom = Random.Range(0, _floors[i].BurningRooms.Count);
+                if (!ArsonisRooms.Contains(_floors[i].BurningRooms[namberRoom]))
+                {
+                    ArsonisRooms.Add(_floors[i].BurningRooms[namberRoom]);
+                    _floors[i].BurningRooms[namberRoom].ActivationFair();
+                }
             }
+
         }
     }
     [ContextMenu("SearchBurningRoom")]
     private void SearchBurningRoom()
     {
-        _burningRooms = FindObjectsOfType<BurningRoom>();
+        for (int i = 0; i < _floors.Length; i++)
+        {
+            _floors[i].BurningRooms = new List<BurningRoom>();
+            for (int j = 0; j < _floors[i].Stage.childCount; j++)
+            {
+                BurningRoom room = _floors[i].Stage.GetChild(j).GetComponent<BurningRoom>();
+                if(room!=null)
+                _floors[i].BurningRooms.Add(room);
+            }
+        }
     }
 }
