5391724f4d01e5ef36c515c7e7d0c3c6da8525e3
Valentin <kibirevvalik202@gmail.com>
2020-10-26 18:49:51 +0300

    [update] Arsonist

--- FirefighterGame/Assets/Scripts/Building/Arsonist.cs
+++ FirefighterGame/Assets/Scripts/Building/Arsonist.cs
@@ -7,45 +7,82 @@ public class Arsonist : MonoBehaviour
     [System.Serializable]
     private struct Floor
     {
-        public Transform Stage;
-        public List<BurningRoom> BurningRooms;
+        public Stage Stageobj;
+        public List<BurningRoom> NoBurningRooms;
         public int ArsonisNamber;
     }
 
     [SerializeField]
     private Floor[] _floors;
+    [SerializeField]
+    private bool[] _percentage = new bool[100];
+    [SerializeField]
+    private float _timeBeforeArson;
+    [SerializeField]
+    [Range(1, 100)]
+    private int _percentageOfFireInTheUpperRoom;
+    private int _numberFloor = int.MinValue;
+    private void Awake()
+    {
+        PercentSetting();
+        for (int i = 0; i < _floors.Length; i++)
+        {
+            _floors[i].Stageobj.Initialization(this, i);
+        }
+    }
     void Start()
     {
-
-        RandomArsonis();
+        StartCoroutine(RandomArsonis());
     }
 
     void Update()
     {
-        
-    }
-    private void RandomArsonis()
-    {
-        for (int i = 0; i < _floors.Length; i++)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            List<BurningRoom> ArsonisRooms = new List<BurningRoom>();
+            _numberFloor++;
+        }
+    }
+    private IEnumerator RandomArsonis()
+    {
 
-            while (ArsonisRooms.Count < _floors[i].ArsonisNamber)
+        while (true)
+        {
+            if (LevelManager.IsStartGame&&_numberFloor>=0)
             {
-                if (_floors[i].ArsonisNamber > _floors[i].BurningRooms.Count)
+                int floorNumberVariation;
+                if (_percentage[Random.Range(0, _percentage.Length)] && (_numberFloor + 1) < _floors.Length)
                 {
-                    Debug.Log("request exceeds the number of rooms, maximum number of rooms " + _floors[i].BurningRooms.Count + " namber "+i);
-                    break;
+                    floorNumberVariation = _numberFloor + 1;
+                }
+                else
+                {
+                    floorNumberVariation = _numberFloor;
                 }
 
-                int namberRoom = Random.Range(0, _floors[i].BurningRooms.Count);
-                if (!ArsonisRooms.Contains(_floors[i].BurningRooms[namberRoom]))
+                int numberRoom = 0;
+
+                if (_floors[floorNumberVariation].NoBurningRooms.Count > 0)
                 {
-                    ArsonisRooms.Add(_floors[i].BurningRooms[namberRoom]);
-                    _floors[i].BurningRooms[namberRoom].ActivationFair();
+                    numberRoom = Random.Range(0, _floors[floorNumberVariation].NoBurningRooms.Count);
                 }
+                else if (CheckFloor())
+                {
+                    yield return new WaitForSeconds(_timeBeforeArson);
+                    continue;
+                }
+                else
+                {
+                    continue;
+                }
+
+                _floors[floorNumberVariation].NoBurningRooms[numberRoom].ActivationFair();
+                _floors[floorNumberVariation].NoBurningRooms.Remove(_floors[floorNumberVariation].NoBurningRooms[numberRoom]);
+                yield return new WaitForSeconds(_timeBeforeArson);
             }
-
+            else
+            {
+                yield return new WaitForSeconds(Time.fixedDeltaTime);
+            }
         }
     }
     [ContextMenu("SearchBurningRoom")]
@@ -53,13 +90,49 @@ public class Arsonist : MonoBehaviour
     {
         for (int i = 0; i < _floors.Length; i++)
         {
-            _floors[i].BurningRooms = new List<BurningRoom>();
-            for (int j = 0; j < _floors[i].Stage.childCount; j++)
+            _floors[i].NoBurningRooms = new List<BurningRoom>();
+            for (int j = 0; j < _floors[i].Stageobj.transform.childCount; j++)
             {
-                BurningRoom room = _floors[i].Stage.GetChild(j).GetComponent<BurningRoom>();
-                if(room!=null)
-                _floors[i].BurningRooms.Add(room);
+                BurningRoom room = _floors[i].Stageobj.transform.GetChild(j).GetComponent<BurningRoom>();
+                if (room != null)
+                    _floors[i].NoBurningRooms.Add(room);
             }
         }
+    }
+    private bool CheckFloor()
+    {
+        int namber = _numberFloor;
+        if ((_numberFloor + 1) < _floors.Length)
+        {
+            namber++;
+        }
+        for (int i = _numberFloor; i < namber; i++)
+        {
+            if ((_floors[namber].NoBurningRooms.Count > 0))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    private void PercentSetting()
+    {
+        List<int> nambers = new List<int>();
+        for (int i = 0; i < 100; i++)
+        {
+            nambers.Add(i);
+        }
+        int quantity = 0;
+        while (quantity < _percentageOfFireInTheUpperRoom)
+        {
+            int i = nambers[Random.Range(0, nambers.Count)];
+            _percentage[i] = true;
+            nambers.Remove(i);
+            quantity++;
+        }
+    }
+    public void NextNumber(int numberFloor)
+    {
+        _numberFloor = numberFloor;
     }
 }
---
+++ FirefighterGame/Assets/Scripts/Building/Stage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage : MonoBehaviour
+{
+    private Arsonist _arsonist;
+    private int _numberFloor;
+    
+    public void Initialization(Arsonist arsonist,int number)
+    {
+        _numberFloor = number;
+        _arsonist = arsonist;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "MainCamera")
+        {
+            _arsonist.NextNumber(_numberFloor);
+        }
+    }
+}
